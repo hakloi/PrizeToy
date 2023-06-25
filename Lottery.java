d61b53745e82d6c2eb2b5779efe1d4269c050b3c
Violetta <murasakitta@gmail.com>
2023-06-25 09:37:37 +0300

    update

--- Lottery.java
+++ Lottery.java
@@ -5,12 +5,12 @@ public class Lottery {
         Toy toy1 = new Toy(1, "Huggy Wuggy", 10, 20);
         Toy toy2 = new Toy(2, "Doll", 20, 40);
         Toy toy3 = new Toy(3, "LEGO", 30, 10);
-        Toy toy4 = new Toy(3, "Teddy Bear", 40, 80);
-        Toy toy5 = new Toy(3, "Cinema ticket", 50, 50);
-        Toy toy6 = new Toy(3, "Magazine", 60, 60);
-        Toy toy7 = new Toy(3, "Multi-pulti Buba", 80, 70);
-        Toy toy8 = new Toy(3, "Blocks", 90, 90);
-        Toy toy9 = new Toy(3, "Board game", 70, 30);
+        Toy toy4 = new Toy(4, "Teddy Bear", 40, 80);
+        Toy toy5 = new Toy(5, "Cinema ticket", 50, 50);
+        Toy toy6 = new Toy(6, "Magazine", 60, 60);
+        Toy toy7 = new Toy(7, "Multi-pulti Buba", 80, 70);
+        Toy toy8 = new Toy(8, "Blocks", 90, 90);
+        Toy toy9 = new Toy(9, "Board game", 70, 30);
 
 
         store.addToy(toy1);
